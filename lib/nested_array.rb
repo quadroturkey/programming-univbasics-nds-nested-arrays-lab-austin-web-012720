747793b9c350a847c6997e4a06e86e22f43c6d0c
Reese M Bowman <reesemb1@gmail.com>
2019-12-17 04:32:51 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -18,7 +18,10 @@ CONVENTIONAL_PRODUCE = [
 
 def assembled_matrix
   
-  storage_room = []
+  storage_room = [
+    CONVENTIONAL_PRODUCE,
+    ORGANIC_PRODUCE
+    ]
   
   # Build an array that contains both of the above arrays
   # This matrix will represent a produce storage room
